14834dafdd0a8379275d88a3b14bb8c9941a25d0
Brandon Hazelton <hazeltonbw@gmail.com>
2022-06-10 00:05:59 -0700

    upgrade to React 18

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./components/Home";
 import "./App.scss";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <div>hi</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
