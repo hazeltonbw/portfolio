a12ca1274518aaf5364e5ed4f910ab27dfcd1ffa
Brandon Hazelton <hazeltonbw@gmail.com>
2022-07-05 19:48:32 -0700

    centralize global css to App.scss, delete index.css

--- src/shared/StyledComponents.js
+++ src/shared/StyledComponents.js
@@ -5,16 +5,12 @@ export const MainContainer = styled.main`
   flex-direction: column;
 `;
 
-export const FlexColumnContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
 export const Flex = styled.div`
   display: flex;
   align-items: center;
   column-gap: 0.5rem;
 `;
+
 export const Logo = styled.a`
   font-family: "Marck Script", cursive;
   user-select: none;
