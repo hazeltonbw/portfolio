97d8291e27c2e27c239d48357e81b71efb984d6a
Brandon Hazelton <hazeltonbw@gmail.com>
2022-07-10 00:00:25 -0700

    get rid of dot

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,25 +1,25 @@
-import { Flex, Hero, Card } from "./Home";
-import avatar from "../../assets/img/avatar-transparent.svg";
-import { BsArrowRight } from "react-icons/bs";
+import { Flex, Hero, Card } from './Home'
+import avatar from '../../assets/img/avatar-transparent.svg'
+import { BsArrowRight } from 'react-icons/bs'
 
 const Home = () => {
   return (
-    <Flex id="home">
+    <Flex id='home'>
       <Hero>
         <Card>
           <h1>Hello! My name is</h1>
           <h1>
             <strong>Brandon Hazelton</strong>
           </h1>
-          <h1>I am a full-stack web developer.</h1>
-          <a href="#about">
+          <h1>I am a full-stack web developer</h1>
+          <a href='#about'>
             Learn more &nbsp;
-            <BsArrowRight style={{ verticalAlign: "bottom" }} />
+            <BsArrowRight style={{ verticalAlign: 'bottom' }} />
           </a>
         </Card>
-        <img src={avatar} alt="avatar" />
+        <img src={avatar} alt='avatar' />
       </Hero>
     </Flex>
-  );
-};
-export default Home;
+  )
+}
+export default Home
